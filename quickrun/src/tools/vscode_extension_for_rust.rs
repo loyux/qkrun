7075caadf7896edaa920b3f2010805df7dac0d33
loyu <lito0210@outlook.com>
2022-11-16 08:52:01 +0000

    update qkrun tools

--- quickrun/src/tools/vscode_extension_for_rust.rs
+++ quickrun/src/tools/vscode_extension_for_rust.rs
@@ -1,23 +1,25 @@
+use std::process::Stdio;
+
 use tracing::info;
 
 //TO make A quickly environment for rust development
 //快速安装开发rust所需插件,code命令vscode-client才可以使用
 pub fn rust_extensions_install() {
     let extensions = r#"bungcip.better-toml
-    errorlens
-    CoenraadS.bracket-pair-colorizer-2
-    dustypomerleau.rust-syntax
-    evgeniypeshkov.syntax-highlighter
-    gitpod.gitpod-remote-ssh
-    JScearcy.rust-doc-viewer
-    lanza.lldb-vscode
-    lorenzopirro.rust-flash-snippets
-    nyxiative.rust-and-friends
-    PolyMeilex.rust-targets
-    rust-lang.rust-analyzer
-    serayuzgur.crates
-    vadimcn.vscode-lldb
-    ZhangYue.rust-mod-generator"#;
+errorlens
+CoenraadS.bracket-pair-colorizer-2
+dustypomerleau.rust-syntax
+evgeniypeshkov.syntax-highlighter
+gitpod.gitpod-remote-ssh
+JScearcy.rust-doc-viewer
+lanza.lldb-vscode
+lorenzopirro.rust-flash-snippets
+nyxiative.rust-and-friends
+PolyMeilex.rust-targets
+rust-lang.rust-analyzer
+serayuzgur.crates
+vadimcn.vscode-lldb
+ZhangYue.rust-mod-generator"#;
     let pd: Vec<&str> = extensions.split("\n").into_iter().collect();
     for ii in pd {
         info!("Installing {}", ii);
@@ -25,7 +27,10 @@ pub fn rust_extensions_install() {
         std::process::Command::new("code")
             .arg("--install-extension")
             .arg(ii)
-            .output()
+            .stderr(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            // .output()
+            .spawn()
             .unwrap();
     }
 }
